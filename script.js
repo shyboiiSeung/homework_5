4d6100b956167288709b6fb98a61440184066220
shyboiiSeung <163094594+shyboiiSeung@users.noreply.github.com>
2024-05-27 19:25:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function calculateTotal() {
-    var americanoPrice = 4500;
-    var lattePrice = 5000;
-    var smoothiePrice = 5500;
-    var milkshakePrice = 5000;
-    var grapefruitPrice = 4000;
+    let americanoPrice = 4500;
+    let lattePrice = 5000;
+    let smoothiePrice = 5500;
+    let milkshakePrice = 5000;
+    let grapefruitPrice = 4000;
     
-    var americanoQty = document.getElementById('americano').value;
-    var latteQty = document.getElementById('latte').value;
-    var smoothieQty = document.getElementById('smoothie').value;
-    var milkshakeQty = document.getElementById('milkshake').value;
-    var grapefruitQty = document.getElementById('grapefruit').value;
+    let americanoQty = document.getElementById('americano').value;
+    let latteQty = document.getElementById('latte').value;
+    let smoothieQty = document.getElementById('smoothie').value;
+    let milkshakeQty = document.getElementById('milkshake').value;
+    let grapefruitQty = document.getElementById('grapefruit').value;
     
-    var total = (americanoPrice * americanoQty) +
+    let total = (americanoPrice * americanoQty) +
                 (lattePrice * latteQty) +
                 (smoothiePrice * smoothieQty) +
                 (milkshakePrice * milkshakeQty) +
